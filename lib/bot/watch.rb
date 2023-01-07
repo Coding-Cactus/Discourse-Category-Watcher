2aeb762dbf5bd0a5dbc83c72aceed5e74c8e08d0
Coding Cactus <65423356+Coding-Cactus@users.noreply.github.com>
2023-01-07 16:58:24 +0000

    🐛 Fix bug with multiple channels watching a category

--- lib/bot/watch.rb
+++ lib/bot/watch.rb
@@ -1,6 +1,9 @@
 class Bot
     def watch(event, domain, category_id, discord_channel)
         domain.downcase!
+        category_id     = category_id.to_s
+        discord_channel = discord_channel.to_i
+        
         client = Discourse::Client.new(domain)
 
         begin
@@ -24,19 +27,35 @@ class Bot
                 domain: domain,
                 last_id: 0,
                 categories: {
-                    category_id => [discord_channel.to_i]
+                    category_id => [discord_channel]
                 }
             })
         else
             categories = data[:categories]
 
-            if categories.include?(category_id)
-                categories[category_id] << discord_channel.to_i
-            else                
-                categories[category_id] = [discord_channel.to_i]
-            end
+            p categories
 
-            @domains.update_one({ domain: domain }, { "$set" => { categories: categories } })
+            if categories.include?(category_id)
+                if categories[category_id].include?(discord_channel)
+                    event.respond(
+                        embeds: [Discordrb::Webhooks::Embed.new(
+            				title: "Already Watching!",
+            				description: "I am already watching https://#{domain}/c/-/#{category_id} in <##{discord_channel}>",
+            				colour: 0xcc0000,
+            				timestamp: Time.new
+            			)]
+                    )
+                    return
+                end
+
+                p "categories.#{category_id}"
+
+                p [domain, category_id, discord_channel]
+                
+                @domains.update_one({ domain: domain }, { "$push" => { "categories.#{category_id}" => discord_channel } })
+            else                
+                @domains.update_one({ domain: domain }, { "$set"  => { "categories.#{category_id}" => [discord_channel] } })
+            end
         end
 
         event.respond(
