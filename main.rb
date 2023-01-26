e6a2ca8601695b35a0e290f82bcf67374cf7eed8
Coding Cactus <65423356+Coding-Cactus@users.noreply.github.com>
2023-01-26 19:29:05 +0000

    🚀 Prepare for Railway

--- main.rb
+++ main.rb
@@ -1,17 +1,9 @@
 require "http"
 require "json"
 require "mongo"
-require "sinatra"
 require "discordrb"
 
 require_relative "lib/bot"
 require_relative "lib/discourse"
 
-set :bind, "0.0.0.0"
-set :protection, :except => :frame_options
-
-get "/" do
-	"hi"
-end
-
-Thread.new { Bot.new(ENV["token"], ENV["mongouri"]).run }
+Bot.new(ENV["token"], ENV["mongouri"]).run
